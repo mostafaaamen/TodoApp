de1237ff88b38e8e64cbd05d5e544694e8bdecfd
mostafaaamen <88153348+mostafaaamen@users.noreply.github.com>
2023-03-11 16:03:00 +0200

    set login/ads.txt

--- index.js
+++ index.js
@@ -26,7 +26,12 @@ app.get("*", (req, res) => {
        res.sendFile(path.join(__dirname, '/public', 'index.html'));
 
 })
-app.get("/login", (req, res) => {
+app.get("/login/Ads.txt", (req, res) => {
+
+       res.sendFile(path.join(__dirname, '/public', 'Ads.txt'));
+
+})
+app.get("/login/ads.txt", (req, res) => {
 
        res.sendFile(path.join(__dirname, '/public', 'Ads.txt'));
 
