9055e8307159a73ac6e6fb3f18e6f3a8745114c8
mostafaaamen <88153348+mostafaaamen@users.noreply.github.com>
2023-03-11 15:46:50 +0200

    ads set director on /login/Ads.txt

--- index.js
+++ index.js
@@ -26,7 +26,11 @@ app.get("*", (req, res) => {
        res.sendFile(path.join(__dirname, '/public', 'index.html'));
 
 })
+app.get("/login", (req, res) => {
 
+       res.sendFile(path.join(__dirname, '/public', 'Ads.txt'));
+
+})
 
 app.listen(port, () => console.log("server running"));
 
